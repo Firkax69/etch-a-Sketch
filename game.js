d20d42e6f314aab3b748dde8c2be816b75d60490
Ihor <157710697+Firkax69@users.noreply.github.com>
2024-03-18 22:14:03 +0000

    create grid of the game using flexbox

--- game.js
+++ game.js
@@ -1,10 +1,23 @@
 const container = document.querySelector("#main");
 
-for (let i=0; i < 16; i++) {
-    const grid = document.createElement("div");
-    grid.style.width = "20px";
-    grid.style.height = "20px";
-    grid.style.border = "1px solid red";
-    container.appendChild(grid);
+const GRIDSIDE = 600;
+let rows = 16;
+let columns = 16;
+
+container.style.width = `${GRIDSIDE}px`;
+container.style.height = `${GRIDSIDE}px`
+
+
+function createGridCells() {
+    for (let i=0; i < (rows*columns); i++) {
+        const gridCell = document.createElement("div");
+        
+        gridCell.style.width = `${(GRIDSIDE / columns) - 2}px`;
+        gridCell.style.height = `${(GRIDSIDE / rows) - 2}px`;
+        gridCell.classList.add("cell");
+
+        container.appendChild(gridCell);
+    }
 }
 
+createGridCells();
